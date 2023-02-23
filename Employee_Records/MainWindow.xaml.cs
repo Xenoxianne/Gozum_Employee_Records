d2e14e6096a9a239779852720af9a4d9aa366e33
Denise Gozum <69788360+Xenoxianne@users.noreply.github.com>
2023-02-23 22:25:26 +0800

    Code cleanup

--- Employee_Records/MainWindow.xaml.cs
+++ Employee_Records/MainWindow.xaml.cs
@@ -12,9 +12,6 @@ using System.Windows;
 
 namespace Employee_Records
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
